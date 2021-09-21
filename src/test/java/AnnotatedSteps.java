d04c29461313675feb6405de126a94c7140d7e67
antonyakusenko <mnbv0@rambler.ru>
2021-09-21 12:32:56 +0400

    First

---
+++ src/test/java/AnnotatedSteps.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class AnnotatedSteps {
+    private final static String REPOSITORY = "allure-notifications";
+    private final static String ISSUE_NAME = "Add launch user name";
+
+    @Test
+    public void testGithub(){
+        WebSteps step = new WebSteps();
+        step.openMainPage();
+        step.searchForRepository(REPOSITORY);
+        step.goToRepository(REPOSITORY);
+        step.openIssuesTab();
+        step.shouldHaveName(ISSUE_NAME);
+    }
+}
---
+++ src/test/java/Selenide.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.startMaximized;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class Selenide {
+
+    @BeforeAll
+    static void beforeAll() {
+        startMaximized = true;
+        baseUrl = "https://github.com";
+    }
+
+    @Test
+    public void testRepositoryIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open(baseUrl);
+        $("[placeholder='Search GitHub']").val("selenide/selenide").submit();
+        $(byLinkText("selenide/selenide")).click();
+        $(byPartialLinkText("Issues")).click();
+        $(withText("#1561")).shouldBe(Condition.visible);
+    }
+}
